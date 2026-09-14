9e4241516526c390592a2378476ea823839e3eb8
ranabirjashu3-code <ranabirjashu3@gmail.com>
2026-09-14 22:17:11 +0530

    continueWatching and profile setup

--- Streamify/server/server.js
+++ Streamify/server/server.js
@@ -5,32 +5,34 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/users.js";
 import watchlistRoutes from "./routes/watchlist.js";
 import watchHistoryRouter from "./routes/watchHistory.js";
+import tmdbRoutes from "./routes/tmdbRoutes.js";
 import dns from "dns";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/users", userRoutes);
-app.use("/api/watchlist", watchlistRoutes);
-app.use("/api/watch-history", watchHistoryRouter);
-
 
 dotenv.config();
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/watchlist", watchlistRoutes);
+app.use("/api/watch-history", watchHistoryRouter);
+app.use("/api/tmdb", tmdbRoutes);
+
+
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection failed:", err);
-  });
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection failed:", err);
+    });
 
-app.listen(8080, ()=>{
+
+app.listen(8080, () => {
     console.log("Streamify server is running...");
-    
 });
-
